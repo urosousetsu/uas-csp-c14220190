3ccb07b7cd8d1a1cfb579aefde01192aabdebb32
Bill <billy.timoti@gmail.com>
2025-06-21 12:42:37 +0700

    update eslint for deploymrnt

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,15 +4,12 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center md:flex-row md:justify-between">
           <div className="text-gray-400 text-sm">
-            © {new Date().getFullYear()} Urosousetsu A.K.A Billy Timotius . All rights reserved.
+            © {new Date().getFullYear()} UAS CLIENT SITE BY BILLY TIMOTIUS C14220190. All rights reserved.
           </div>
           <div className="mt-4 md:mt-0 flex space-x-6">
             <a href="https://github.com/urosousetsu" className="text-gray-400 hover:text-gray-300 text-sm">
               Bill's Github
             </a>
-            <p className="text-gray-400 text-sm">
-              UAS CLIENT SIDE C14220190
-            </p>
           </div>
         </div>
       </div>
